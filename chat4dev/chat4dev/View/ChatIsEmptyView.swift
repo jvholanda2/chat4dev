ae11b6cdf1a49f1120d35fd253bd15608bcd671e
vitor <jvholanda18@gmail.com>
2024-07-18 11:57:47 -0300

    Continua a implementaçao da listagem de chats

---
+++ chat4dev/chat4dev/View/ChatIsEmptyView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatIsEmptyView.swift
+//  chat4dev
+//
+//  Created by João Vitor Alves Holanda on 18/07/24.
+//
+
+import SwiftUI
+
+struct ChatIsEmptyView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatIsEmptyView()
+}
---
+++ chat4dev/chat4dev/ViewModel/ListChatViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ListChatViewModel.swift
+//  chat4dev
+//
+//  Created by João Vitor Alves Holanda on 18/07/24.
+//
+
+import Foundation
