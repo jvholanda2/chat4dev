84e258a39e57ac0a5d5fa144dbee7e35394f82bb
vitor <jvholanda18@gmail.com>
2024-07-18 09:54:55 -0300

    Adiciona Login, e inicia a listagem de chats reaproveitando a tela do coredata

---
+++ chat4dev/chat4dev/View/ListChatsView.swift
@@ -0,0 +1,18 @@
+//
+//  ListChatsView.swift
+//  chat4dev
+//
+//  Created by João Vitor Alves Holanda on 18/07/24.
+//
+
+import SwiftUI
+
+struct ListChatsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ListChatsView()
+}
